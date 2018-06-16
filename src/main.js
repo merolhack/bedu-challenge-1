990bf509cfa6e29d20d8673a1045f1506191681e
Lenin Meza <merolhack@gmail.com>
2018-06-16 13:50:11 -0500

    Delete cards with jQuery

--- src/main.js
+++ src/main.js
@@ -15,25 +15,13 @@ document.body.appendChild(root);
 
 // jQuery document ready
 $(document).ready(function () {
-    $(".buddy").on("swiperight", function () {
-        $(this).addClass('rotate-left').delay(700).fadeOut(1);
-        $('.buddy').find('.status').remove();
-
-        $(this).append('<div class="status like">Like!</div>');
-        if ($(this).is(':last-child')) {
-            $('.buddy:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(300);
-        } else {
-            $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-        }
+    console.log($(".buddy"));
+    $('#like').on('click', function () {
+        $('#card1').addClass('rotate-left').delay(700).fadeOut(1);
+        $('#buddy1').find('.status').remove();
     });
-    $(".buddy").on("swipeleft", function () {
-        $(this).addClass('rotate-right').delay(700).fadeOut(1);
-        $('.buddy').find('.status').remove();
-        $(this).append('<div class="status dislike">Dislike!</div>');
-        if ($(this).is(':last-child')) {
-            $('.buddy:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(300);
-        } else {
-            $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-        }
+    $('#dislike').on('click', function () {
+        $('#card2').addClass('rotate-right').delay(700).fadeOut(1);
+        $('#buddy2').find('.status').remove();
     });
 });
