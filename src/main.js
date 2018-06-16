65e0a316dbb76ca82c81c82f1f6bcdcdfcc5a522
Lenin Meza <merolhack@gmail.com>
2018-06-16 13:13:33 -0500

    HTML and SASS imported

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import * as $ from "jquery";
 import './main.scss';
 import 'bootstrap';
 
+// Load the HTML
+import cards from './cards.html';
+import buttons from './buttons.html';
+
+// Create and append the HTML to body
+const root = document.createElement("div");
+root.className = 'content-fluid';
+root.innerHTML = cards + buttons;
+document.body.appendChild(root);
+
 console.log($);
